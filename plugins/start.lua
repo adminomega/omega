adcdfb87dd8c8cf56e7ad17372480fba38cb7bbb
adminomega <1dgrecnd@30mail.ir>
2016-01-22 17:58:02 +0330

    Create start.lua

---
+++ plugins/start.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "برای خرید گروه به این ای دی مراجعه کنید @Unkownhacker"
+end
+
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/start$",
+    "^!Start$",
+    "^/Start$",
+    "^!start$",
+   "^!help$",
+  "^/help$",
+  "^!Help$",
+  },
+  run = run
+}
+
+end
